04fc6aa08f446ece9bddf366f7f816a81225543a
satorricol <satorricol@gmail.com>
2022-01-11 22:22:17 -0400

    share page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { MainComponent } from './pages/main/main.component';
+import { ShareComponent } from './pages/share/share.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     NavbarComponent,
-    MainComponent
+    MainComponent,
+    ShareComponent
   ],
   imports: [
     BrowserModule
