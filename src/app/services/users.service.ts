8f5b1fff18191f6e2af9bcfc623423cee292766f
H0OE <rodrigoquiroz308@gmail.com>
2022-01-15 23:39:06 -0400

    test

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -8,7 +8,7 @@ import {User} from "../models/user";
 export class UsersService {
 
   selectedUser?:User;
-  readonly URI_API = "https://186.121.251.3:4500/~shoplist"   //TODO
+  readonly URI_API = "/~shoplist"   //TODO
 
   constructor(private http:HttpClient) {
     this.selectedUser = new User()
